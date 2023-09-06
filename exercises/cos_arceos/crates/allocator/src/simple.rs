ea5a2534b4023c7add8c3212e161d1084b3540ad
ZhiyuanSue <2262387848@qq.com>
2023-09-06 20:33:18 +0800

    finish all

--- exercises/cos_arceos/crates/allocator/src/simple.rs
+++ exercises/cos_arceos/crates/allocator/src/simple.rs
@@ -4,45 +4,74 @@
 
 use core::alloc::Layout;
 use core::num::NonZeroUsize;
-
+use crate::bitmap::*;
+use crate::AllocError;
+const PAGE_SIZE:usize = 4096;
 use crate::{AllocResult, BaseAllocator, ByteAllocator};
+use crate::PageAllocator;
 
-pub struct SimpleByteAllocator;
+pub struct SimpleByteAllocator
+{
+    start:usize,
+    end:usize,
+    allocation:usize,
+    next:usize,
+}
 
 impl SimpleByteAllocator {
     pub const fn new() -> Self {
-        Self {}
+        Self {
+            start:0,
+            end:0,
+            allocation:0,
+            next:0,
+        }
     }
 }
 
 impl BaseAllocator for SimpleByteAllocator {
     fn init(&mut self, _start: usize, _size: usize) {
-        todo!();
+        self.start=_start;
+        self.end=_start+_size;
+        self.allocation=0;
+        self.next=_start;
     }
 
     fn add_memory(&mut self, _start: usize, _size: usize) -> AllocResult {
-        todo!();
+        Err(AllocError::NoMemory) // unsupported
     }
 }
 
 impl ByteAllocator for SimpleByteAllocator {
     fn alloc(&mut self, _layout: Layout) -> AllocResult<NonZeroUsize> {
-        todo!();
+        let start=(self.next+(_layout.align()-1))&!(_layout.align()-1);
+        if start + _layout.size() > self.end {
+            Err(AllocError::NoMemory)
+        }
+        else
+        {
+            self.allocation+=1;
+            self.next=start+_layout.size();
+            Ok(NonZeroUsize::new(start).unwrap())
+        }
     }
 
     fn dealloc(&mut self, _pos: NonZeroUsize, _layout: Layout) {
-        todo!();
+        self.allocation=self.allocation-1;
+        if self.allocation==0{
+            self.next=self.start;
+        }
     }
 
     fn total_bytes(&self) -> usize {
-        todo!();
+        self.end-self.start
     }
 
     fn used_bytes(&self) -> usize {
-        todo!();
+        self.next-self.start
     }
 
     fn available_bytes(&self) -> usize {
-        todo!();
+        self.end-self.next
     }
 }
